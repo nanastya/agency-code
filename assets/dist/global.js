f2aadc98161611b0af309b2fc17171b5133d16c2
Nastya <anastasia.klimenko@beetroot.se>
2017-09-25 21:06:28 +0300

    Flip card, animation

--- assets/dist/global.js
+++ assets/dist/global.js
@@ -9,6 +9,21 @@ this.activeTarget=b,this.clear();var c=this.selector+'[data-target="'+b+'"],'+th
 
 (function ($) {
     // Scripts that will only run once the page Document Object Model (DOM) is ready for JavaScript code to execute.
+
+    function isScrolledIntoView(elem) {
+        var offset = ($(window).height() - $(elem).height() ) / 2;
+        if (elem.length) {
+            var docViewTop = $(window).scrollTop();
+            var docViewBottom = docViewTop + $(window).height();
+
+            var elemTop = $(elem).offset().top;
+            var elemBottom = elemTop + $(elem).height();
+
+            return ((elemBottom <= ( docViewBottom + offset )) && (elemTop >= ( docViewTop + offset )) );
+        }
+        return false;
+    }
+
     $(document).ready(function () {
         var headerHeight = $('.header').outerHeight();
         var navHeight = $('.header__navigation').outerHeight();
@@ -21,6 +36,13 @@ this.activeTarget=b,this.clear();var c=this.selector+'[data-target="'+b+'"],'+th
             $(this).toggleClass('menu-icon--open');
             $('.header__menu').toggleClass('header__menu--show');
             $('.header__logo').toggleClass('header__logo--transform');
+        });
+
+        $( window ).scroll(function() {
+            var image = $('.txt-cols-img__image');
+            if ( isScrolledIntoView(image) ) {
+                image.addClass('txt-cols-img__image--animated');
+            }
         });
 
     });
